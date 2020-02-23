7254723c0fa9b89d959c380897f59db8d3ea7eb6
beingwalflower <52874469+beingwalflower@users.noreply.github.com>
2020-02-23 19:19:39 +0530

    Create program1_welcome.c

---
+++ program1_welcome.c
@@ -0,0 +1,8 @@
+/*printf is used to display the message on the output scren. C has a file stdio in which the input and output commands are defined*/
+#include <stdio.h>     
+int main()           
+{
+  printf("Welcome to my hub\n");
+  return 0;
+}
+
